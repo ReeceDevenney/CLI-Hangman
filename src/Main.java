4bbbac4d823b43e5badc299f8b0a08d01a635f7d
reece <reecedevenney@gmail.com>
2023-09-11 12:29:49 -0400

    incorrect guesses count is now tracked

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import java.util.Objects;
 
 public class Main {
 
-    static void gameLoop(String phrase, String secret) throws IOException {
+    static void gameLoop(String phrase, String secret, int incorrectCount) throws IOException {
         System.out.println(secret);
+        System.out.println(incorrectCount + "/5 incorrect guesses");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Guess a letter: ");
 
         String guess = reader.readLine();
         String holder = "";
+        boolean correctCheck = false;
 
         for (int i = 0; i < phrase.length(); i++) {
             if(secret.charAt(i) == ' ') {
@@ -19,16 +21,24 @@ public class Main {
                 holder = holder + secret.charAt(i);
             } else if (phrase.charAt(i) == guess.charAt(0)) {
                 holder = holder + guess;
+                correctCheck = true;
             } else {
                 holder = holder + '*';
             }
         }
+
+        if(!correctCheck) {
+            incorrectCount += 1;
+        }
+
         secret = holder;
         if(secret.equals(phrase)) {
             System.out.println("the answer was " + phrase);
             System.out.println("You got it!");
+        } else if (incorrectCount == 5){
+            System.out.println("Too many wrong guesses, you lose");
         } else {
-            gameLoop(phrase, secret);
+            gameLoop(phrase, secret, incorrectCount);
         }
     }
     public static void main(String[] args) throws IOException{
@@ -44,6 +54,7 @@ public class Main {
             secret = secret + '*';
         }
         }
-        gameLoop(phrase, secret);
+        int incorrectCount = 0;
+        gameLoop(phrase, secret, incorrectCount);
     }
 }
