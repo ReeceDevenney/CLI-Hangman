bfca135d6e4adea44b4d283a394144c918e8c35c
reece <reecedevenney@gmail.com>
2023-09-11 12:22:29 -0400

    player can now guess letters

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
 import java.io.*;
+import java.util.Objects;
 
 public class Main {
+
+    static void gameLoop(String phrase, String secret) throws IOException {
+        System.out.println(secret);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Guess a letter: ");
+
+        String guess = reader.readLine();
+        String holder = "";
+
+        for (int i = 0; i < phrase.length(); i++) {
+            if(secret.charAt(i) == ' ') {
+                holder = holder + ' ';
+            } else if(secret.charAt(i) != '*') {
+                holder = holder + secret.charAt(i);
+            } else if (phrase.charAt(i) == guess.charAt(0)) {
+                holder = holder + guess;
+            } else {
+                holder = holder + '*';
+            }
+        }
+        secret = holder;
+        if(secret.equals(phrase)) {
+            System.out.println("the answer was " + phrase);
+            System.out.println("You got it!");
+        } else {
+            gameLoop(phrase, secret);
+        }
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("What is your phrase?");
@@ -14,6 +44,6 @@ public class Main {
             secret = secret + '*';
         }
         }
-        System.out.println(secret);
+        gameLoop(phrase, secret);
     }
 }
